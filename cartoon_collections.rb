4b74463927e7c78b2cf79d78e7e9b91444dd3751
Will Saffran <willh.saffran@gmail.com>
2019-02-08 21:03:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,7 +23,8 @@ def long_planeteer_calls(calls)
   return truth
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
 end
